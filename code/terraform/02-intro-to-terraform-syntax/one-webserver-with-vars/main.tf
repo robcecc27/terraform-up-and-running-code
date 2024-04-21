70a718cb19596fc383a34d9f6d56194422c16cdd
Rob Cecchini <rob.cecchini@gmail.com>
2024-04-21 10:41:32 -0500

    changed all AMI references to us-east-1 AMI's

--- code/terraform/02-intro-to-terraform-syntax/one-webserver-with-vars/main.tf
+++ code/terraform/02-intro-to-terraform-syntax/one-webserver-with-vars/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
+  ami                    = "ami-04e5276ebb8451442"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
